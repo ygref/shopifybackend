309e7e1320120e22537605c1f86a555c4ea82f60
ygref <jason.wesley.hc@gmail.com>
2024-04-24 16:29:55 -0400

    api call working, multi item info

--- server.js
+++ server.js
@@ -11,6 +11,16 @@ app.get("/api/items", (req, res) => {
   res.json(itemsData);
 });
 
+app.get("/api/items/barcode/:barcode", (req, res) => {
+  const barcode = req.params.barcode;
+  const item = itemsData.find((item) => item.barcode === barcode);
+  if (item) {
+    res.json(item);
+  } else {
+    res.status(404).json({ error: "Item Not Found" });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
