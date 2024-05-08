52f5fedf067425d8be67b3a27e944c6833f1c1ec
ygref <jason.wesley.hc@gmail.com>
2024-05-08 13:57:44 -0400

    check before change

--- server.js
+++ server.js
@@ -4,22 +4,31 @@ const app = express();
 const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 
-const itemsData = JSON.parse(fs.readFileSync("items.json", "utf-8"));
+const shipmentData = JSON.parse(fs.readFileSync("shipment.json", "utf-8"));
 
 app.use(cors());
-app.get("/api/items", (req, res) => {
-  res.json(itemsData);
+app.get("/api/shipment", (req, res) => {
+  res.json(shipmentData);
 });
 
-app.get("/api/items/barcode/:barcode", (req, res) => {
-  const barcode = req.params.barcode;
-  const item = itemsData.find((item) => item.barcode === barcode);
-  if (item) {
-    res.json(item);
+app.get("/api/shipment/:id", (req, res) => {
+  const shipmentId = req.params.id;
+  const shipment = shipmentData.find((shipment) => shipment.shipmentId === shipmentId);
+  if (shipment) {
+    res.json(shipment);
   } else {
-    res.status(404).json({ error: "Item Not Found" });
+    res.status(404).json({ error: "Shipment not Found" });
   }
 });
+// app.get("/api/items/barcode/:barcode", (req, res) => {
+//   const barcode = req.params.barcode;
+//   const item = itemsData.find((item) => item.barcode === barcode);
+//   if (item) {
+//     res.json(item);
+//   } else {
+//     res.status(404).json({ error: "Shipment not Found" });
+//   }
+// });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
